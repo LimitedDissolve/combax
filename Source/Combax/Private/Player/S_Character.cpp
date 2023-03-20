2a4794779260b208b211fcd6be808521386b6877
moaria <dreampassion@yandex.ru>
2023-03-20 23:32:35 +0300

    feat: removed useless onjumped function

--- Source/Combax/Private/Player/S_Character.cpp
+++ Source/Combax/Private/Player/S_Character.cpp
@@ -12,11 +12,10 @@
 #include "Runtime/Launch/Resources/Version.h"
 #include "HAL/IConsoleManager.h"
 
-static TAutoConsoleVariable<int32> CVarJumpBoost(TEXT("sv.jump_boost"), 1, TEXT("If the player should boost in a movement direction while jumping.\n0 - disables jump boosting entirely\n1 - boosts in the direction of input, even when moving in another direction\n2 - boosts in the direction of input when moving in the same direction\n"), ECVF_Default);
 static TAutoConsoleVariable<int32> CVarBunnyhop(TEXT("sv.bunnyhopping"), 0, TEXT("Enable normal bunnyhopping.\n"), ECVF_Default);
 
 // Sets default values
-AS_Character::AS_Character(const FObjectInitializer& ObjectInitializer)
+AS_Character::AS_Character(const FObjectInitializer &ObjectInitializer)
 	: Super(ObjectInitializer.SetDefaultSubobjectClass<US_CharacterMovement>(ACharacter::CharacterMovementComponentName))
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -25,26 +24,26 @@ AS_Character::AS_Character(const FObjectInitializer& ObjectInitializer)
 	GetCapsuleComponent()->InitCapsuleSize(30.48f, 68.58f);
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Block);
 
-	// Create a CameraComponent	
+	// Create a CameraComponent
 	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
 	FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f)); // Position the camera
 	FirstPersonCameraComponent->bUsePawnControlRotation = true;
-	
+
 	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
 	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
 	Mesh1P->SetOnlyOwnerSee(true);
 	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
 	Mesh1P->bCastDynamicShadow = false;
 	Mesh1P->CastShadow = false;
-	//Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
+	// Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
 	Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
 }
 
 void AS_Character::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+
 	if (bDeferJumpStop)
 	{
 		bDeferJumpStop = false;
@@ -58,9 +57,9 @@ void AS_Character::BeginPlay()
 	Super::BeginPlay();
 	MaxJumpTime = -4.0f * GetCharacterMovement()->JumpZVelocity / (3.0f * GetCharacterMovement()->GetGravityZ());
 
-	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	if (APlayerController *PlayerController = Cast<APlayerController>(Controller))
 	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		if (UEnhancedInputLocalPlayerSubsystem *Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
 		{
 			Subsystem->AddMappingContext(DefaultMappingContext, 0);
 		}
@@ -68,39 +67,39 @@ void AS_Character::BeginPlay()
 }
 
 //~ Called to bind functionality to input
-void AS_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AS_Character::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	
+
 	// Set up action bindings
-	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	if (UEnhancedInputComponent *EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		//Jumping
+		// Jumping
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
-		//Moving
+		// Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AS_Character::Move);
 
-		//Looking
+		// Looking
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AS_Character::Look);
 	}
 }
 
-void AS_Character::Move(const FInputActionValue& Value)
+void AS_Character::Move(const FInputActionValue &Value)
 {
 	// input is a Vector2D
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
 	if (Controller != nullptr)
 	{
-		// add movement 
+		// add movement
 		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
 		AddMovementInput(GetActorRightVector(), MovementVector.X);
 	}
 }
 
-void AS_Character::Look(const FInputActionValue& Value)
+void AS_Character::Look(const FInputActionValue &Value)
 {
 	// input is a Vector2D
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
@@ -110,66 +109,6 @@ void AS_Character::Look(const FInputActionValue& Value)
 		// add yaw and pitch input to controller
 		AddControllerYawInput(LookAxisVector.X);
 		AddControllerPitchInput(LookAxisVector.Y);
-	}
-}
-
-void AS_Character::OnJumped_Implementation()
-{
-	const int32 JumpBoost = CVarJumpBoost->GetInt();
-
-	if (GetWorld()->GetTimeSeconds() >= LastJumpBoostTime + MaxJumpTime && JumpBoost)
-	{
-		LastJumpBoostTime = GetWorld()->GetTimeSeconds();
-		// Boost forward speed on jump
-		FVector Facing = GetActorForwardVector();
-		// Use input direction
-		FVector Input = GetCharacterMovement()->GetCurrentAcceleration();
-		if (JumpBoost != 1)
-		{
-			// Only boost input in the direction of current movement axis (prevents ABH).
-			Input *= FMath::Max(Input.GetSafeNormal2D() | GetCharacterMovement()->Velocity.GetSafeNormal2D(), 0.0f);
-		}
-		float ForwardSpeed = Input | Facing;
-		// Adjust how much the boost is
-		float SpeedBoostPerc = bIsSprinting || bIsCrouched ? 0.1f : 0.5f;
-		// How much we are boosting by
-		float SpeedAddition = FMath::Abs(ForwardSpeed * SpeedBoostPerc);
-		// We can only boost up to this much
-		float MaxBoostedSpeed = GetCharacterMovement()->GetMaxSpeed() + GetCharacterMovement()->GetMaxSpeed() * SpeedBoostPerc;
-		// Calculate new speed
-		float NewSpeed = SpeedAddition + GetMovementComponent()->Velocity.Size2D();
-		float SpeedAdditionNoClamp = SpeedAddition;
-
-		// Scale the boost down if we are going over
-		if (NewSpeed > MaxBoostedSpeed)
-		{
-			SpeedAddition -= NewSpeed - MaxBoostedSpeed;
-		}
-
-		if (ForwardSpeed < -MovementPtr->GetMaxAcceleration() * FMath::Sin(0.6981f))
-		{
-			// Boost backwards if we're going backwards
-			SpeedAddition *= -1.0f;
-			SpeedAdditionNoClamp *= -1.0f;
-		}
-
-		// Boost our velocity
-		FVector JumpBoostedVel = GetMovementComponent()->Velocity + Facing * SpeedAddition;
-		float JumpBoostedSizeSq = JumpBoostedVel.SizeSquared2D();
-		if (CVarBunnyhop.GetValueOnGameThread() != 0)
-		{
-			FVector JumpBoostedUnclampVel = GetMovementComponent()->Velocity + Facing * SpeedAdditionNoClamp;
-			float JumpBoostedUnclampSizeSq = JumpBoostedUnclampVel.SizeSquared2D();
-			if (JumpBoostedUnclampSizeSq > JumpBoostedSizeSq)
-			{
-				JumpBoostedVel = JumpBoostedUnclampVel;
-				JumpBoostedSizeSq = JumpBoostedUnclampSizeSq;
-			}
-		}
-		if (GetMovementComponent()->Velocity.SizeSquared2D() < JumpBoostedSizeSq)
-		{
-			GetMovementComponent()->Velocity = JumpBoostedVel;
-		}
 	}
 }
 
@@ -240,12 +179,11 @@ void AS_Character::OnMovementModeChanged(EMovementMode PrevMovementMode, uint8 P
 	MovementModeChangedDelegate.Broadcast(this, PrevMovementMode, PrevCustomMode);
 }
 
-
 void AS_Character::RecalculateBaseEyeHeight()
 {
-	const ACharacter* DefaultCharacter = GetClass()->GetDefaultObject<ACharacter>();
+	const ACharacter *DefaultCharacter = GetClass()->GetDefaultObject<ACharacter>();
 	const float OldUnscaledHalfHeight = DefaultCharacter->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight();
-	const UCapsuleComponent* CharacterCapsule = GetCapsuleComponent();
+	const UCapsuleComponent *CharacterCapsule = GetCapsuleComponent();
 	const float CurrentUnscaledHalfHeight = CharacterCapsule->GetUnscaledCapsuleHalfHeight();
 	const float CurrentAlpha = 1.0f - CurrentUnscaledHalfHeight;
 	BaseEyeHeight = FMath::Lerp(DefaultCharacter->BaseEyeHeight, CrouchedEyeHeight, SimpleSpline(CurrentAlpha));
